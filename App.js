96fdd154b560aed1721ac0ea96467b3885e19f75
Harshit <harshitchouksey2200@gmail.com>
2025-04-14 20:33:58 +0530

    First commit L1

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child_1" }, [
+    React.createElement("h1", { key: "FC1" }, "I am first child"),
+    React.createElement("h2", { key: "FC2" }, "I am second child"),
+  ]),
+  React.createElement("div", { id: "child_2" }, [
+    React.createElement("h1", { key: "FC1" }, "I am first child"),
+    React.createElement("h2", { key: "FC2" }, "I am second child"),
+  ]),
+]);
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
